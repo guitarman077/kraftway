f85d1865d86619adfc01c535ff34ff47bee61b20
istepanov <stepanov.ilya@gtrf.ru>
2015-09-01 17:11:58 +0300

    1.3

--- 1/1,2,3.sql
+++ 1/1,2,3.sql
@@ -14,7 +14,6 @@ JOIN City AS ci ON l.id_city = ci.id
 
 -- Топ 5 стран (по количеству житеелй)
 -- прим: используем LEFT JOIN на случай если стран имеющих жителей буедт менее 5
-
 SELECT
   c.country
 FROM Country AS c 
@@ -22,3 +21,12 @@ LEFT JOIN Location AS l ON c.id = l.id_country
 GROUP BY l.id_country
 ORDER BY COUNT(l.id) DESC, c.country -- сортируем по c.country чтобы страны с равным количеством жителей выводились по алфавиту 
 LIMIT 5
+
+-- Города в которых не проживают жители
+-- прим: выбрал такой вариант так как работает во много раз выстрее чем варинт с GROUP BY ... HAVING COUNT(...) = 0
+SELECT 
+  c.city
+FROM City AS c
+LEFT JOIN Location AS l ON c.id = l.id_city
+WHERE l.id IS NULL
+
