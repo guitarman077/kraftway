e8141794dfc08d020c94e3f4b4c464764011cc17
istepanov <stepanov.ilya@gtrf.ru>
2015-09-01 18:08:31 +0300

    2.2

--- 2/1.php
+++ 2/1.php
@@ -1,4 +1,4 @@
-<?
+<?php
 /*
  * Для выполнения второй части задания будем использовать Doctrine ORM первой версии
  *
@@ -6,13 +6,19 @@
  * С учетом того что в рамках данного задание нами не предпологается наличие каких либо методов в моделей описывать выше перечисленные классы я счел излишним
  * особенно если учесть что Doctrine ORM может сгенерировать данные класс автоматически.
  */
-?>
 
-<?
 // Вывод пользователей и их гео данных
-
 Doctrine_Query::create()
     ->select('u.name, u.email, c.country, r.region, ci.city')
     ->from('User u, u.Location l, l.Country c, l.Region r, l.City ci')
     ->execute();
-?>
+
+// Топ 5 стран (по количеству житеелй)
+Doctrine_Query::create()
+    ->select('c.country, COUNT(l.id)')
+    ->from('Country c, c.Location l')
+    ->groupBy('l.id_country')
+    ->orderBy('COUNT(l.id)')
+    ->limit(5)
+    ->excute();
+
